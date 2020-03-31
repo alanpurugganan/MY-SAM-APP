4b71476aaf130a1ce2018501f45474394980e2e2
Alan Purugganan <alan.purugganan@gmail.com>
2020-03-31 18:29:15 -0400

    added more stuff

--- my-sam-app-nodejs/hello-world/app.js
+++ my-sam-app-nodejs/hello-world/app.js
@@ -5,7 +5,7 @@ let AWS = require("aws-sdk");
 let response;
 let dynamodb = new AWS.DynamoDB();
 
-if(process.env.EnvType == 'Local'){
+if(process.env.EnvType == '__Local__'){
     let config = {
       "region":"local",
       "endpoint": `http://${process.env.LocalIp}:${process.env.LocalDynamoDbPort}`
@@ -14,7 +14,7 @@ if(process.env.EnvType == 'Local'){
     dynamodb = new AWS.DynamoDB(config);
 }
 
-exports.lambdaHandler = async (event, context) => {
+exports.helloWorld = async (event, context) => {
     try {
         // const ret = await axios(url);
         response = {
@@ -44,3 +44,23 @@ exports.lambdaHandler = async (event, context) => {
 
     return response;
 };
+
+exports.helloWorld2 = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: `hello world2`
+                // location: ret.data.trim()
+            })
+        }
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response;
+};
+
